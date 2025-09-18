f1883a66ab66466ac9018c29ae0c591e8427314f
smilecomunicacion <141651179+smilecomunicacion@users.noreply.github.com>
2025-09-18 11:17:24 +0200

    Revert "Ensure block editor loads theme styling"

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -26,13 +26,7 @@ function smile_v6_setup() {
 	add_theme_support( 'post-thumbnails' );
         add_theme_support( 'appearance-tools' );
         add_theme_support( 'editor-styles' );
-        add_editor_style(
-                array(
-                        'assets/css/editor-style.css',
-                        'style.css',
-                        'assets/css/main.css',
-                )
-        );
+        add_editor_style( 'assets/css/editor-style.css' );
 
 	// Register nav menus.
 	register_nav_menus(
